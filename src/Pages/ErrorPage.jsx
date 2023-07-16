6c7faf57ed3295c34913f9f67708fa7074bcc5d9
MRHemel <hemelrahman80@gmail.com>
2023-07-16 21:00:55 +0600

    title icon added

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -1,9 +1,11 @@
 
+import { Link } from 'react-router-dom';
 import error from '../assets/404.json'
 import Lottie from "lottie-react";
 const ErrorPage = () => {
     return (
-        <div className='mt-0 scr'>
+        <div>
+            <Link to={'/'}><span className='bg-blue-300 border-8 rounded-2xl p-4 '>go back to home</span></Link>
             <Lottie className=' w-1/2 mx-auto' loop={true} animationData={error}></Lottie>
         </div>
     );
