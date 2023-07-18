f42eb31a0b30ab0951a0920d7c208070143f1c9d
MRHemel <hemelrahman80@gmail.com>
2023-07-18 13:48:08 +0600

    footer correction

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,32 +3,35 @@ import { FaLanguage } from "react-icons/fa";
 
 const Footer = () => {
     return (
-        <footer className="footer p-10 bg-[#19376D] text-white ">
-            <div>
-                <a className="btn btn-ghost normal-case lg:text-3xl "> <FaLanguage className="lg:text-5xl"></FaLanguage> World Language </a>
-                <p>World Language Ltd.<br />Providing language teaching since 1992</p>
-            </div>
-            <div>
-                <span className="footer-title">Services</span>
-                <a className="link link-hover">Branding</a>
-                <a className="link link-hover">Design</a>
-                <a className="link link-hover">Marketing</a>
-                <a className="link link-hover">Advertisement</a>
-            </div>
-            <div>
-                <span className="footer-title">Company</span>
-                <a className="link link-hover">About us</a>
-                <a className="link link-hover">Contact</a>
-                <a className="link link-hover">Jobs</a>
-                <a className="link link-hover">Press kit</a>
-            </div>
-            <div>
-                <span className="footer-title">Legal</span>
-                <a className="link link-hover">Terms of use</a>
-                <a className="link link-hover">Privacy policy</a>
-                <a className="link link-hover">Cookie policy</a>
-            </div>
-        </footer>
+        <div>
+            <footer className="footer p-10 bg-[#19376D] text-white ">
+                <div>
+                    <a className="btn btn-ghost normal-case lg:text-3xl "> <FaLanguage className="lg:text-5xl"></FaLanguage> World Language </a>
+                    <p>World Language Ltd.<br />Providing language teaching since 1992</p>
+                </div>
+                <div>
+                    <span className="footer-title">Services</span>
+                    <a className="link link-hover">Branding</a>
+                    <a className="link link-hover">Design</a>
+                    <a className="link link-hover">Marketing</a>
+                    <a className="link link-hover">Advertisement</a>
+                </div>
+                <div>
+                    <span className="footer-title">Company</span>
+                    <a className="link link-hover">About us</a>
+                    <a className="link link-hover">Contact</a>
+                    <a className="link link-hover">Jobs</a>
+                    <a className="link link-hover">Press kit</a>
+                </div>
+                <div>
+                    <span className="footer-title">Legal</span>
+                    <a className="link link-hover">Terms of use</a>
+                    <a className="link link-hover">Privacy policy</a>
+                    <a className="link link-hover">Cookie policy</a>
+                </div>
+            </footer>
+            <p className="bg-[#19376D] text-center text-white pb-4">Copyright © 2023 - All right reserved</p>
+        </div>
     );
 };
 
